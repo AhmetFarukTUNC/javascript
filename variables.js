06e50f16050f0d5a022c3d500c93207f60c654d5
YAZILIMCIABII <129683968+AhmetFarukTUNC@users.noreply.github.com>
2023-10-30 08:22:48 +0300

    Variables
    We did simple example about variables topic.

---
+++ variables.js
@@ -0,0 +1,58 @@
+//A example for let keyword...
+
+let x1 = 7;
+
+let y1 = 3;
+
+let z1 = x1 + y1;
+
+console.log("Answer 1 : " + z1);
+
+// A example for const(fixed) keyword
+
+const x2 = 7;
+
+const y2 = 3;
+
+const z2 = x2 + y2;
+
+console.log("Answer 2 : " + z2)
+
+// A example for let keyword
+
+let x3 = 4;
+
+let y3 = 3;
+
+let z3 = x3 + y3;
+
+console.log("Answer 3 : " +z3)
+
+// We defined an x ​​that has no value.
+
+let x;
+
+console.log("x is " + x);
+
+// A random example
+
+let firstNumber = 7;
+
+let secondNumber = 3;
+
+let firstName = "Michael";
+
+let firstZipCode = "75082";
+
+let secondZipCode ="60459";
+
+console.log(firstNumber);
+
+console.log(secondNumber);
+
+console.log(firstName);
+
+console.log(firstZipCode);
+
+console.log(secondZipCode);
+
