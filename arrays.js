7c6973f7046a143ff6757ca13ca975bbb806a72d
YAZILIMCIABII <129683968+AhmetFarukTUNC@users.noreply.github.com>
2023-11-03 00:08:32 +0300

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,42 @@
+let a = [4,8,15,16,23,42];
+
+let b = ["David","Eddie","Alex","Michael"];
+
+console.log(a[0]);
+
+console.log(a[1]);
+
+console.log(a);
+
+a[0] =7;
+
+console.log(a);
+
+let c = [4,"Alex",true];
+
+console.log(c);
+
+console.log( typeof c);
+
+console.log(c[9])
+
+console.log(a.length);
+
+a[10] = 77;
+
+console.log(a);
+
+console.log(a.length);
+
+a.push(32)
+
+console.log(a);
+
+console.log(a.length);
+
+a.pop();
+
+console.log(a);
+
+console.log(a.length);
+
